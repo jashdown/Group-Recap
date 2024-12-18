9e907057414d7e59105075250ff479f7d934f51f
Justin Ashdown <justin.m.ashdown@gmail.com>
2024-12-18 09:38:49 -0500

    able to change the order of rich text and markdown components

--- components/Post.tsx
+++ components/Post.tsx
@@ -4,41 +4,45 @@ import { RecapPage } from "@/types/contentful";
 import { useContentfulInspectorMode, useContentfulLiveUpdates } from "@contentful/live-preview/react";
 import moment from "moment";
 import { Markdown } from "./Markdown";
-import { RichText } from "./RIchText";
+import { RichText } from "./RichText";
 
 export const Post = ({ page }: { page: RecapPage }) => {
   const updatedPage = useContentfulLiveUpdates(page);
   const inspectorProps = useContentfulInspectorMode({ entryId: page.sys.id });
   
   const {
-      name,
-      bookOfTheBible,
-      chapter,
-      date,
-      notes,
-      notesMarkdown
-    } = updatedPage.fields;
+    name,
+    bookOfTheBible,
+    chapter,
+    date,
+    notes,
+    notesMarkdown,
+    richTextFirst,
+  } = updatedPage.fields;
   
-    return (
-      <div id="main">
-        <div className="inner">
-          <header id="header" {...inspectorProps({ fieldId: 'name' })}>{name}</header>
-          <section>
-            <header className="main">
-              <h1>
-                <span {...inspectorProps({ fieldId: 'bookOfTheBible' })}>{bookOfTheBible}</span>
-                <span {...inspectorProps({ fieldId: 'chapter' })}>{chapter}</span>
-              </h1>
-              {date && <p {...inspectorProps({ fieldId: 'date' })}>{moment(date).format('MMMM Do, YYYY')}</p>}
-            </header>
-            {notes && <RichText notes={notes} {...inspectorProps({
-                fieldId: 'notes',
-              })}/>}
-            {notesMarkdown && <Markdown notes={notesMarkdown} {...inspectorProps({
-                fieldId: 'notesMarkdown',
-              })}/>}
-          </section>
-        </div>
+  return (
+    <div id="main">
+      <div className="inner">
+        <header id="header" {...inspectorProps({ fieldId: 'name' })}>{name}</header>
+        <section>
+          <header className="main">
+            <h1>
+              <span {...inspectorProps({ fieldId: 'bookOfTheBible' })}>{bookOfTheBible}</span>
+              <span {...inspectorProps({ fieldId: 'chapter' })}>{chapter}</span>
+            </h1>
+            {date && <p {...inspectorProps({ fieldId: 'date' })}>{moment(date).format('MMMM Do, YYYY')}</p>}
+          </header>
+          {richTextFirst && notes && <RichText notes={notes} {...inspectorProps({
+              fieldId: 'notes',
+            })}/>}
+          {notesMarkdown && <Markdown notes={notesMarkdown} {...inspectorProps({
+              fieldId: 'notesMarkdown',
+            })}/>}
+          {!richTextFirst && notes && <RichText notes={notes} {...inspectorProps({
+              fieldId: 'notes',
+            })}/>}
+        </section>
       </div>
-    );
+    </div>
+  );
 }
